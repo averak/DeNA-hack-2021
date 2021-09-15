e641a50329ff180f85c17711e7c0b47870f57583
吉井駿一 <syun_syun01@icloud.com>
2021-09-16 02:01:35 +0900

    不要なpagesを削除

--- frontend/src/pages/404.tsx
+++
@@ -1,7 +0,0 @@
-import type { NextPage } from "next";
-
-const ErrorPage: NextPage = () => {
-  return <h1>404</h1>;
-};
-
-export default ErrorPage;
--- frontend/src/pages/500.tsx
+++
@@ -1,7 +0,0 @@
-import type { NextPage } from "next";
-
-const ErrorPage: NextPage = () => {
-  return <h1>500</h1>;
-};
-
-export default ErrorPage;
