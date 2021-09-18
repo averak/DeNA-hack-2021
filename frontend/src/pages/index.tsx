0a5983a98ceae7d5b7c0dc8d903e35c8e0167968
hide <nkret805worm@gmail.com>
2021-09-18 15:41:00 +0900

    UPDATE: 不要なアイコン削除

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -39,25 +39,6 @@ const DownIcon: VFC<IconProps> = ({ className }) => {
   );
 };
 
-const SearchIcon: VFC<IconProps> = ({ className }) => {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      className={className}
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke="currentColor"
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="2"
-        d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-      />
-    </svg>
-  );
-};
-
 type AreaSelectType = {
   category: string;
   areas: { [key: string]: string };
