bc6d1f577bbde160651bd6252fc19b267cdad7b9
averak <abe12@mccc.jp>
2021-09-18 20:02:02 +0900

    [feat] add trip plans list get api response body interface

---
+++ app/src/main/java/dev/abelab/hack/dena/api/response/TripPlanResponse.java
@@ -0,0 +1,63 @@
+package dev.abelab.hack.dena.api.response;
+
+import java.util.List;
+
+import lombok.*;
+import dev.abelab.hack.dena.model.TripPlanItemModel;
+import dev.abelab.hack.dena.model.TripPlanAttachmentSubmitModel;
+
+/**
+ * 旅行プランレスポンス
+ */
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class TripPlanResponse {
+
+    /**
+     * 旅行プランID
+     */
+    Integer id;
+
+    /**
+     * タイトル
+     */
+    String title;
+
+    /**
+     * 説明文
+     */
+    String description;
+
+    /**
+     * 作成者
+     */
+    UserResponse author;
+
+    /**
+     * 都道府県ID
+     */
+    Integer regionId;
+
+    /**
+     * いいね数
+     */
+    Integer likes;
+
+    /**
+     * 添付ファイル
+     * */
+    TripPlanAttachmentSubmitModel attachment;
+
+    /**
+     * タグリスト
+     */
+    List<String> tags;
+
+    /**
+     * 項目リスト
+     */
+    List<TripPlanItemModel> items;
+
+}
