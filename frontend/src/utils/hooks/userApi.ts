ba3bb06b2b9d6e34a9d7fde86812a538c6e5fef4
chamegashi <msymsy0693wrb@gmail.com>
2021-09-18 22:23:52 +0900

    escape localStrage

--- frontend/src/utils/hooks/userApi.ts
+++ frontend/src/utils/hooks/userApi.ts
@@ -3,9 +3,9 @@ import { useCallback, useState } from "react";
 
 const hostname = process.env.NEXT_PUBLIC_BASE_URI;
 
-const tokenType = localStorage.getItem("tokenType");
-const accessToken = localStorage.getItem("accessToken");
-axios.defaults.headers.common["Autorization"] = `${tokenType} ${accessToken}`;
+// const tokenType = localStorage.getItem("tokenType");
+// const accessToken = localStorage.getItem("accessToken");
+// axios.defaults.headers.common["Autorization"] = `${tokenType} ${accessToken}`;
 
 export type SignUpParam = {
   email: string;
