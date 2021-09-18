5611bf7526cd9b7e74a29d30643b54080c03e615
吉井駿一 <syun_syun01@icloud.com>
2021-09-18 13:51:36 +0900

    ヘッダーコンポーネントを実装

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,5 +1,45 @@
+import { ChevronRightIcon, HomeIcon, UserIcon } from "@heroicons/react/solid";
+import Link from "next/link";
 import type { VFC } from "react";
 
-export const Header: VFC = () => {
-  return <div>Header Component</div>;
+import type { PathData } from "./Layout";
+
+type HeaderProps = {
+  title: string;
+  pathList: PathData[];
+};
+
+export const Header: VFC<HeaderProps> = (props) => {
+  return (
+    <header className="fixed z-50 w-screen">
+      <div className="flex justify-between w-screen bg-blue-200">
+        <Link href="/">
+          <a className="block align-middle">Logo</a>
+        </Link>
+        <h1 className="block">{props.title}</h1>
+        <Link href="/user/mypage">
+          <a className="block">
+            <UserIcon className="w-10 h-10" />
+          </a>
+        </Link>
+      </div>
+      <div className="flex justify-start w-screen bg-white">
+        <Link href="/">
+          <a className="m-0">
+            <HomeIcon className="w-5 h-5" />
+          </a>
+        </Link>
+        {props.pathList.map((path, i) => {
+          return (
+            <div key={i} className="flex justify-start">
+              <ChevronRightIcon className="w-5 h-5" />
+              <Link href={path.pathLink}>
+                <a>{path.pathTitle}</a>
+              </Link>
+            </div>
+          );
+        })}
+      </div>
+    </header>
+  );
 };
--- frontend/src/pages/user/mypage.tsx
+++ frontend/src/pages/user/mypage.tsx
@@ -1,7 +1,16 @@
 import type { VFC } from "react";
+import type { PathData } from "src/components/Layout";
+import { Layout } from "src/components/Layout";
 
 const MyPage: VFC = () => {
-  return <main>MyPage</main>;
+  const pathList: PathData[] = [
+    { pathTitle: "マイページ", pathLink: "/user/mypage" },
+  ];
+  return (
+    <Layout title="マイページ" pathList={pathList}>
+      <main>MyPage</main>
+    </Layout>
+  );
 };
 
 export default MyPage;
