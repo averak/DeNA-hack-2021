702691ddb825169018fff75d63591734d7552d23
chamegashi <msymsy0693wrb@gmail.com>
2021-09-18 22:19:19 +0900

    can request login and signup

--- frontend/src/pages/user/register.tsx
+++ frontend/src/pages/user/register.tsx
@@ -1,14 +1,35 @@
 import type { VFC } from "react";
-import { AuthField } from "src/components/User/AuthField";
+import { RegistAuthField } from "src/components/User/AuthField";
+
+import type { SignUpParam } from "../../utils/hooks/userApi";
+import { useSignUp as UseSignUp } from "../../utils/hooks/userApi";
 
 const UserRegisterPage: VFC = () => {
-  const authLogin = () => {
+  // const { getFn, response } = UseSignUp();
+  const { getFn } = UseSignUp();
+
+  const authResiter = (
+    firstName: string,
+    lastName: string,
+    mail: string,
+    password: string
+  ) => {
+    const param: SignUpParam = {
+      firstName: firstName,
+      lastName: lastName,
+      email: mail,
+      password: password,
+    };
+
+    getFn(param).then(() => {
+      // console.log(response)
+    });
     return;
   };
   return (
     <div className="flex relative justify-center items-center w-full h-screen bg-blue-500">
-      <div className="w-full max-w-[320px] h-auto">
-        <AuthField submitText="新規登録" handleSubmit={authLogin} />
+      <div className="w-full h-auto max-w-[320px]">
+        <RegistAuthField submitText="新規登録" handleSubmit={authResiter} />
       </div>
     </div>
   );
